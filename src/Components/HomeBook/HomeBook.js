ddeb8cb1f695b81fcf58028b0f9230b15f6e2639
mattscript <ayobamifagoroye@yahoo.com>
2023-08-08 19:42:02 +0100

    typographical error

--- src/Components/HomeBook/HomeBook.js
+++ src/Components/HomeBook/HomeBook.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Slider from "react-slick";
-import Typography from "@material-ui/core/typography";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import "slick-carousel/slick/slick.css";
